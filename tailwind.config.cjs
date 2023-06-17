b9f45f0f1191660decbd773d94f3491d18a3901d
lrag14 <laurentrober@gmail.com>
2023-06-18 01:23:01 +0200

    header Navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			        xl: { max: '1200px' },
+			   xl: { max: '1200px' },
             lg: { max: '991px' },
             md: { max: '767px' },
-            sm: { max: '550px' },
-            xsm: { max: '375px' },
+            // sm: { max: '550px' },
+            // xsm: { max: '375px' },
 
 		},
 	},
