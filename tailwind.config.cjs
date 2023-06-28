0dc36d60b2ed4b25d852f333e2cbf19f1eece512
lrag14 <laurentrober@gmail.com>
2023-06-28 11:00:01 +0200

    clean

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			   xl: { max: '1200px' },
-            lg: { max: '991px' },
-            md: { max: '767px' },
-            // sm: { max: '550px' },
-            // xsm: { max: '375px' },
-
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        'almost-white': 'hsl(0,0,98%)',
+        'mediunm-gray': 'hsl(0,0,41%)',
+        'almost-black': 'hsl(0,0,8%)',
+      },
+    },
+  },
+  plugins: [],
+};
