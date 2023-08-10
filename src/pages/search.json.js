82805aaa11f19dabc503e9902d2c7a12e8423440
lrag14 <laurentrober@gmail.com>
2023-08-10 09:02:13 +0200

    deleted search

--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,14 +1,14 @@
-import { getPage } from 'astro:content';
+// import { getPage } from 'astro:content';
 
-export async function get() {
-  const pageContent = await getPage('page');
-  return {
-    body: {
-      pageContent,
-    },
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    status: 200,
-  };
-}
+// export async function get() {
+//   const pageContent = await getPage('page');
+//   return {
+//     body: {
+//       pageContent,
+//     },
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     status: 200,
+//   };
+// }
